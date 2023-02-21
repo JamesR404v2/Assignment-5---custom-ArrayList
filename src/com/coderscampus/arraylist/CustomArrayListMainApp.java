9327eaa6943c71cc3c738cf502e03dd716362498
JamesR404v2 <james.kuijpers@web.de>
2023-02-21 21:13:16 +0100

    still WIP

---
+++ src/com/coderscampus/arraylist/CustomArrayListMainApp.java
@@ -0,0 +1,19 @@
+public class CustomArrayListMain {
+    public static void main(String[] args) {
+        System.out.println("app started.");
+        CustomArrayListMain launch = new CustomArrayListMain();
+        launch.Orchestrator();
+    }
+
+
+    public void Orchestrator()
+    {
+        System.out.println("app launched.");
+
+
+
+
+    }
+
+}
+
